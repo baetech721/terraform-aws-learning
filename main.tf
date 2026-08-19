7f51a7ef86c6c87e21ab195bd199c353e669ff3f
baetech721 <therealestartist@icloud.com>
2026-08-19 03:04:12 -0700

    Configure S3 backend for Terraform state
    Added S3 backend configuration for Terraform state management.

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket = "baetech-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
